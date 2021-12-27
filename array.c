b983a1e87de6e148bbb5b7bd9bd9bde82bd56b7a
Tsheeba <96413723+Tsheeba@users.noreply.github.com>
2021-12-27 11:11:08 +0530

    Update array.c

--- array.c
+++ array.c
@@ -5,15 +5,17 @@ void main()
 {
    int array[10], p, i, n ;
    int *a[10];
+   
 
    printf("Enter number of elements in array\n");
    scanf("%d", &n);
 
    printf("Enter %d elements\n", n);
+  
 
    for (i = 0; i < n; i++)
       scanf("%d", &array[i]);
-
+         a[i]=(int*) malloc(n * sizeof(int));
    printf("Enter the location delete element\n");
    scanf("%d", &p);
 
@@ -24,16 +26,19 @@ void main()
       for (i = p - 1; i < n - 1; i++){
            array[i] = array[i+1];
            a[i]=&array[i];
-           free(a[i]);
+    
          
          }
-
+   free(a[i]);
       printf("Resultant array:\n");
     
 
       for (i = 0; i < n ; i++)
          printf("%d\n", array[i]);
   }
+
+
+}
 
 
 }
